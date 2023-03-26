89e420170cc04feeee992c330e20709c0e49e2fa
rejoice <rejoicegono@gmail.com>
2023-03-26 05:08:38 -0700

    tasks

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -4,3 +4,9 @@ file_line { 'Turn off passwd auth':
   path   => '/etc/ssh/ssh_config',
   line   => '    PasswordAuthentication no',
 }
+
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
+}
